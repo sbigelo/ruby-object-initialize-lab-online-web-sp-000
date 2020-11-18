7cab581556c15d2e03ccc821d49e14400457d4ac
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-18 22:29:58 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,9 @@
 class Person
-  
+  attr_accessor :name
   def initialize(name)
     @name = name
   end
   
-  def name=(name)
-    @name =  name
-  end
-  
-  def name
-    @name
-  end
+ 
   
 end
